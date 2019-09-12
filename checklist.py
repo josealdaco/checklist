5733fac2df455dafa6821928e6da6feceb657baf
siko408 <gamerbloxhd@gmail.com>
2019-09-12 13:45:09 -0700

    Added Remove function

--- checklist.py
+++ checklist.py
@@ -26,29 +26,29 @@ def user_input(prompt):
     return user_input
 
 def select(function_code):
-    if function_code == "C":
+    if function_code == "C" or function_code == "c":
         an = input("Input Item:")
         create(an)
         return True
     # Read item
-    elif function_code == "R":
+    elif function_code == "R" or function_code == "r":
         item_index = int(input("Index Number?:"))
         # Remember that item_index must actually exist or our program will crash.
         read(item_index)
         return True
-    elif function_code == "M":
+    elif function_code == "M" or function_code == "m":
         an = int(input("INPUT INDEX:"))
         mark_completed(an)
         return True
     # Print all items
-    elif function_code == "P":
+    elif function_code == "P" or function_code == "p":
         list_all_items()
         return True
-    elif function_code == "D":
+    elif function_code == "D" or function_code == "d":
         destroy_index = int(input("Index to destroy?:"))
         destroy(destroy_index)
         return True
-    elif function_code == "Q":
+    elif function_code == "Q" or function_code == "q":
         return False
     # Catch all
     else:
@@ -56,12 +56,15 @@ def select(function_code):
     # User Selection Code here
 
 def test():
+    print("This is a testing function")
     select("C")
-
     list_all_items()
+    print("testing reading function..")
     select("R")
     list_all_items()
+    print("testing checkmark function..")
     select("M")
+    print("Test function Ended..")
 # Run Tests
 test()
 
